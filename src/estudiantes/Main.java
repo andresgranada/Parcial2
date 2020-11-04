59ba0acb6327289dc010f6482e180b0cf9fdb687
jairoandres <andres.granada98@gmail.com>
2020-11-04 18:54:26 -0500

    Remove Main comment

--- src/estudiantes/Main.java
+++ src/estudiantes/Main.java
@@ -1,74 +1,9 @@
 package estudiantes;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-
 public class Main {
 
     public static void main(String[] args) {
-//        ArrayList<Double> notasAndres = new ArrayList(){
-//            {
-//                add(2.0);
-//                add(3.0);
-//                add(4.0);
-//                add(5.0);
-//            }
-//        };
-//        ArrayList<Double> notasYusep = new ArrayList(){
-//            {
-//                add(5.0);
-//                add(1.0);
-//                add(4.2);
-//                add(3.5);
-//            }
-//        };
-//        ArrayList<Double> notasLuis = new ArrayList(){
-//            {
-//                add(4.3);
-//                add(1.6);
-//                add(2.9);
-//                add(3.1);
-//            }
-//        };
-//
-//        ArrayList<Double> notasJose = new ArrayList(){
-//            {
-//                add(4.3);
-//                add(1.5);
-//                add(2.9);
-//                add(3.1);
-//            }
-//        };
-//        Nota notas = new Nota(2.0,3.0,4.0,5.0);
-//        System.out.println(notas.toString());
-
-//        Estudiante estudiante = new Estudiante("Andres", "1152", notas.getTodasLasNotas());
-//        System.out.println(estudiante.toString());
-
         SolicitarDatos solicitarDatos = new SolicitarDatos();
         solicitarDatos.menu();
-//        for (int i = 0; i < 2; i++) {
-//            Estudiante estudiante = solicitarDatos.SolicitarDatosNuevoEstudiante();
-//            estudiantes.add(estudiante);
-//        }
-
-//        AccionesParaEstudiante.estudiantes.add(new Estudiante("Luis", "11111", notasLuis));
-//        AccionesParaEstudiante.estudiantes.add(new Estudiante("Andres", "1152", notasAndres));
-//        AccionesParaEstudiante.estudiantes.add(new Estudiante("Yusep", "2234", notasYusep));
-//        AccionesParaEstudiante.estudiantes.add(new Estudiante("Jose", "2222", notasJose));
-
-//        String nombreAborrar = "Andres";
-
-//        AccionesParaEstudiante accionesParaEstudiante = new AccionesParaEstudiante();
-//        accionesParaEstudiante.obtenerMejoresEstudiantes();
-//        System.out.println(AccionesParaEstudiante.estudiantes);
-
-//        solicitarDatos.SolicitarDatosActualizarEstudiante("11111");
-//        System.out.println(AccionesParaEstudiante.estudiantes);
-
-
-
     }
 }
